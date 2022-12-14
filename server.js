1608d50741c6c112d6241f08cd9a820cb1e6a1be
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-12-15 01:05:15 +0530

    add: server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
 console.log("Hello world")
